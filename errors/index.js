5bd200f906c3722e5a18d4e38165bdf83e290818
Jeff Andersen <jeffandersen@gmail.com>
2016-04-14 17:07:56 -0300

    Remove express-dependent methods

--- errors/index.js
+++ errors/index.js
@@ -34,25 +34,6 @@ errors.reject = function(reject) {
 };
 
 /**
- * Respond to an error object via Express
- *
- * @param {object} res - Express response object
- */
-errors.respond = function(res) {
-  res.statusCode = this.statusCode || 500;
-  if (!this.type || res.statusCode === 500) {
-    return res.end();
-  }
-
-  var data = {
-    type: this.type,
-    message: _.isArray(this.message)? this.message : [ this.message ]
-  };
-
-  res.json(`error/${this.type}`, { error: data });
-};
-
-/**
  * Custom error types
  */
 
@@ -62,7 +43,6 @@ errors.NotFound = function NotFound(message) {
   this.message = message;
   this.statusCode = 404;
   this.type = 'not_found';
-  this.respond = errors.respond.bind(this);
 };
 util.inherits(errors.NotFound, Error);
 
@@ -71,7 +51,6 @@ errors.InternalServer = function InternalServer(message) {
   this.name = this.constructor.name;
   this.message = message || 'server error';
   this.statusCode = 500;
-  this.respond = errors.respond.bind(this);
 };
 util.inherits(errors.InternalServer, Error);
 
@@ -81,7 +60,6 @@ errors.BadRequest = function BadRequest(message) {
   this.message = message;
   this.type = 'invalid_request';
   this.statusCode = 400;
-  this.respond = errors.respond.bind(this);
 };
 util.inherits(errors.BadRequest, Error);
 
@@ -91,6 +69,5 @@ errors.Unauthorized = function Unauthorized(message) {
   this.message = message;
   this.statusCode = 401;
   this.type = 'unauthorized';
-  this.respond = errors.respond.bind(this);
 };
 util.inherits(errors.Unauthorized, Error);
