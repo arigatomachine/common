4b449b0330dafa07eed5707e724bc73ba64c7192
Jeff Andersen <jeffandersen@gmail.com>
2016-04-15 12:09:30 -0300

    Extract error types to own file

--- errors/index.js
+++ errors/index.js
@@ -3,7 +3,9 @@
 var errors = exports;
 
 var _ = require('lodash');
-var util = require('util');
+
+var types = require('./types');
+_.extend(errors, types);
 
 /**
  * Catch and reject with database error
@@ -32,42 +34,3 @@ errors.reject = function(reject) {
     reject(err);
   };
 };
-
-/**
- * Custom error types
- */
-
-errors.NotFound = function NotFound(message) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = message;
-  this.statusCode = 404;
-  this.type = 'not_found';
-};
-util.inherits(errors.NotFound, Error);
-
-errors.InternalServer = function InternalServer(message) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = message || 'server error';
-  this.statusCode = 500;
-};
-util.inherits(errors.InternalServer, Error);
-
-errors.BadRequest = function BadRequest(message) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = message;
-  this.type = 'invalid_request';
-  this.statusCode = 400;
-};
-util.inherits(errors.BadRequest, Error);
-
-errors.Unauthorized = function Unauthorized(message) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = message;
-  this.statusCode = 401;
-  this.type = 'unauthorized';
-};
-util.inherits(errors.Unauthorized, Error);
---
+++ errors/types.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var errorTypes = exports;
+
+var util = require('util');
+
+errorTypes.NotFound = function NotFound(message) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = message;
+  this.statusCode = 404;
+  this.type = 'not_found';
+};
+util.inherits(errorTypes.NotFound, Error);
+
+errorTypes.InternalServer = function InternalServer(message) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = message || 'unknown server error';
+  this.statusCode = 500;
+  this.type = 'internal_server';
+};
+util.inherits(errorTypes.InternalServer, Error);
+
+errorTypes.BadRequest = function BadRequest(message) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = message;
+  this.type = 'bad_request';
+  this.statusCode = 400;
+};
+util.inherits(errorTypes.BadRequest, Error);
+
+errorTypes.Unauthorized = function Unauthorized(message) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = message;
+  this.statusCode = 401;
+  this.type = 'unauthorized';
+};
+util.inherits(errorTypes.Unauthorized, Error);
