ebd6801beb79e5249c65f94c43ec4f952206dff0
Ian Livingstone <ianl@cs.dal.ca>
2016-06-27 20:19:39 -0300

    Import user crypto functionality from the CLI

---
+++ crypto/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var crypto = exports;
+
+crypto.user = require('./user');
+crypto.kdf = require('./kdf');
+crypto.utils = require('./utils');
+crypto.triplesec = require('./triplesec');
